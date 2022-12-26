41bc025eaadf2f0ec78c71b8c5e69644a017f958
SamiLekesiz <50396610+SamiLekesiz@users.noreply.github.com>
2022-12-26 17:21:11 +0300

    Create find_schema_size.sql
    source: https://stackoverflow.com/a/63600339/11592839

---
+++ find_schema_size.sql
@@ -0,0 +1,14 @@
+WITH 
+schemas AS (
+SELECT schemaname as name, sum(pg_relation_size(quote_ident(schemaname) || '.' || quote_ident(tablename)))::bigint as size FROM pg_tables
+GROUP BY schemaname
+),
+db AS (
+SELECT pg_database_size(current_database()) AS size
+)
+
+SELECT schemas.name, 
+       pg_size_pretty(schemas.size) as absolute_size,
+       schemas.size::float / (SELECT size FROM db)  * 100 as relative_size
+FROM schemas;
+
